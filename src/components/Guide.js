29679b1e64fb16d3d0c830072a68e71a13c4bb67
Gyana prakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-05-28 20:46:19 +0530

    notes chages

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -1,3 +1,22 @@
-export default function Guide() {
-    
-}
+import { Routes, Route } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import CypressNote from '../config/CypressNote';
+import SeleniumNote from '../config/SeleniumNote';
+import PlaywrightNote from '../config/PlaywrightNote';
+
+export const Guide = () => {
+  return (
+    <div style={{ display: 'flex' }} className='mt-15'>
+      <Sidebar />
+      <div style={{ flex: 1, padding: '1rem' }} className=''>
+        <Routes>
+          <Route path="/" element={<CypressNote/>} />
+          <Route path="/" element={<SeleniumNote/>} />
+          <Route path="/" element={<PlaywrightNote/>} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Guide;
